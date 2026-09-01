306a8540f45b2cae214b40d11a3de112a6989b29
Om Prakash <omprakash.snps@gmail.com>
2026-09-01 21:56:18 +0530

    Time: 44 ms (72.02%), Space: 44.3 MB (82.56%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] A = new int[n+2];
+        A[0]=1;
+        A[n+1]=1;
+        for(int i=0; i<n; i++){
+            A[i+1] = nums[i];
+        }
+        int[][] dp = new int[n+2][n+2];
+
+        for(int len=1; len<=n; len++){
+            for(int i=1; i<=n-len+1; i++){
+                int j = i + len - 1;
+                for(int k=i; k<=j; k++){
+                    int coins = dp[i][k-1]+dp[k+1][j]+A[i-1]*A[k]*A[j+1];
+                    dp[i][j] = Math.max(dp[i][j], coins);
+                }
+            }
+        }
+        return dp[1][n];
+    }
+}
